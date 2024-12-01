d14557783e1c3c2e848bbe3421d031a3ba4b483f
leeyeonseok <leeys4140@naver.com>
2024-12-01 17:29:39 +0900

    Feat: add position publisher

--- src/ROS_remote/position_pub.py
+++ src/ROS_remote/position_pub.py
@@ -1,32 +1,35 @@
-import os, sys
+#!/usr/bin/env python
+import os
+import sys
 import rospy
-from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Float64MultiArray
 
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))) + "/MuJoCo_JW")
+# 현재 파일의 경로를 기준으로 MuJoCo_JW 경로 추가
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../'))
 
 from MuJoCo_JW.dxl import RemoteDynamixel
 
 
-
 class RemotePub:
     def __init__(self) -> None:
-        self.position_pub = rospy.Publisher('positions', Int16MultiArray, queue_size=10)
-        self.remote_dxl = RemoteDynamixel()
+        self.position_pub = rospy.Publisher('positions', Float64MultiArray, queue_size=10)
+        self.remote_dxl = RemoteDynamixel([5, 6, 7, 8, 9])
 
     def position_publisher(self) -> None:
-        msg = Int16MultiArray()
-        rad_list = self.remote_dxl.get_qpos()
-        msg.data = self.remote_dxl.control_pos(rad_list)
+        msg = Float64MultiArray()
+        msg.data = self.remote_dxl.get_qpos()
         self.position_pub.publish(msg)
 
 
 def main():
+    rospy.init_node('remote_pub_node')  # ROS 노드 초기화
+    remote_pub = RemotePub()
     rate = rospy.Rate(200)
-    while rospy.is_shutdown():
-        remote_pub = RemotePub()
+
+    while not rospy.is_shutdown():
         remote_pub.position_publisher()
         rate.sleep()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
